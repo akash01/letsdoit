2bc2ccfd697cb806dd451ec4f323baaaf74abd0c
mukesh <pace.akash@gmail.com>
2014-03-15 01:45:41 +0200

    letsdoit app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letsdoit::Application.config.secret_key_base = '5bfb61723f2836c29495d47165c9dfee21998af61d240409e0eedd4303d9cf41e2492181a2bf942c5006a4101d2639c0a4f2104418f3750eeaf6e1e64f0d9eb6'
