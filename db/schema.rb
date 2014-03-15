b65eaf217a03c120acd65f9d1b0bcbdf8c181022
mukesh <pace.akash@gmail.com>
2014-03-16 00:22:09 +0200

    added user id to comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315171423) do
+ActiveRecord::Schema.define(version: 20140315221950) do
 
   create_table "comments", force: true do |t|
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: true do |t|
