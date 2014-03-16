42d9191a73953772e28cd73bae33f6b7d03247d0
Qianfinland <qianfinland@gmail.com>
2014-03-16 12:04:53 +0200

    heroku

--- db/migrate/20140315171423_rename.rb
+++ db/migrate/20140315171423_rename.rb
@@ -1,6 +1,6 @@
 class Rename < ActiveRecord::Migration
   def change
-    remove_column :posts, :post_id, :integer
+#    remove_column :posts, :post_id, :integer
     remove_column :comments, :postid, :integer
     add_column :comments, :post_id, :integer
   end
